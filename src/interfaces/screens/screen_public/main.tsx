0ca6bb9fa9bee96db0fcbe750384994d000c8a29
antowirantoIO <antowrnto11@gmail.com>
2024-11-13 17:02:55 +0700

    handle submit

--- src/interfaces/screens/screen_public/main.tsx
+++ src/interfaces/screens/screen_public/main.tsx
@@ -2,57 +2,66 @@
 import FormInput from "@/interfaces/components/form-input";
 import { post } from "@/services/api/main_call";
 import { MAIN_ENDPOINT } from "@/services/api/main_endpoint";
-import api from "@/services/api/main_interceptor";
 import type { Inputs } from "@/types/screen_public.types";
 import { Button } from "antd";
 import React, { Fragment } from "react";
 import { useForm } from "react-hook-form";
 
 const ScreenPublic = () => {
-	const {
-		register,
-		control,
-		watch,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<Inputs>({
-		defaultValues: {
-			expiresInMins: 100,
-			username: "emilys",
-			password: "emilyspass",
-		},
-	});
-	const onSubmit = handleSubmit(async (data) => {
-		const { Kind } = await post(MAIN_ENDPOINT.Auth.Login, data);
-		console.log({ Kind });
-	});
-	return (
-		<Fragment>
-			<div className="h-screen flex justify-center items-center">
-				<form onSubmit={onSubmit}>
-					<FormInput
-						label="Username"
-						control={control}
-						name="username"
-						type="text"
-					/>
-					<FormInput
-						label="Password"
-						control={control}
-						name="password"
-						type="password"
-					/>
-					<div className="h-2" />
-					<Button
-						className="bg-slate-800 py-2 px-4 rounded-xl minw-full"
-						htmlType="submit"
-					>
-						Submit
-					</Button>
-				</form>
-			</div>
-		</Fragment>
-	);
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>({
+        defaultValues: {
+            expiresInMins: 100,
+            username: "emilys",
+            password: "emilyspass",
+        },
+    });
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            const { Kind, OK, StatusCode } = await post(MAIN_ENDPOINT.Auth.Login, data);
+            console.log({ OK, StatusCode });
+            if(!OK) {
+                throw new Error();
+            }
+            console.log({ Kind });
+        } catch (error) {
+
+        }
+    });
+    return (
+        <Fragment>
+            <div className="h-screen flex justify-center items-center">
+                <form onSubmit={onSubmit}>
+                    <FormInput
+                        label="Username"
+                        control={control}
+                        name="username"
+                        type="text"
+                        errors={errors}
+                        rules={{ required: true }}
+                    />
+                    <FormInput
+                        label="Password"
+                        control={control}
+                        name="password"
+                        type="password"
+                        errors={errors}
+                        rules={{ required: true }}
+                    />
+                    <div className="h-2" />
+                    <Button
+                        className="bg-slate-800 py-2 px-4 rounded-xl minw-full"
+                        htmlType="submit"
+                    >
+                        Submit
+                    </Button>
+                </form>
+            </div>
+        </Fragment>
+    );
 };
 
 export default ScreenPublic;
